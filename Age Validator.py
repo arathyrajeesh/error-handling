dc59d949cb9fc65d288ac5a55e1deddcbf774f32
Arathy <arathyrajeesh2@gmail.com>
2025-09-16 11:59:29 +0530

    List Index Finder added

--- Age Validator.py
+++ Age Validator.py
@@ -1,6 +1,6 @@
 try:
     age=int(input("Enter your age:"))
-    
+    print(f"User age is :{age}")
     if age<0:
         raise ValueError("Age cannot be negative")
 except ValueError:
--- List Index Finder.py
+++ List Index Finder.py
@@ -1,2 +1,9 @@
+numbers = [10, 20, 30, 40]
+
 try:
-    
+    index = int(input("Enter an index number: "))
+    print("Value at index", index, "is:", numbers[index])
+except IndexError:
+    print("Error: Index out of range. Please enter a valid index (0-3).")
+except ValueError:
+    print("Error: Please enter a valid number.")
